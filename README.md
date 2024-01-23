5ca37d2c1ac1564cb1b8db114d9241a213ce91c7
Orçun Çolak <orcun.colak@hazelcast.com>
2024-01-23 14:23:35 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Backpressure
+The original idea is from  
+https://medium.com/@vikas.taank_40391/managing-back-pressure-in-reactive-streams-c64f91a10adf
---
+++ src/main/java/org/colak/flux/backpressure/BackPressureBufferExample.java
@@ -0,0 +1,40 @@
+package org.colak.flux.backpressure;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.Duration;
+
+@Slf4j
+public class BackPressureBufferExample {
+
+    public static void main(String[] args) throws Exception {
+        Flux<Integer> flux = Flux.range(1, 100)
+                .delayElements(Duration.ofMillis(1))
+                .doOnNext(i -> log.info("Produced: " + i))
+                .onBackpressureBuffer(10, dropped -> log.info("Buffered, dropped: " + dropped));
+
+        flux.subscribeOn(Schedulers.boundedElastic())
+                .subscribe(
+                        data -> {
+                            try {
+                                // Simulating a task that takes 10ms
+                                Thread.sleep(100);
+                                log.info("Consumed: " + data);
+                            } catch (InterruptedException e) {
+                                Thread.currentThread().interrupt();
+                                throw new RuntimeException(e);
+                            }
+                        },
+                        err -> log.error("Error: " + err),
+                        () -> log.info("Completed")
+                );
+
+        // Keep the program running long enough to see the output
+        System.out.println("Print a key to exit");
+        System.in.read();
+        System.exit(0);
+
+    }
+}
